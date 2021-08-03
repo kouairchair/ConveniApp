21659d05d1084f44ce703d49d1db39c06ceb9c33
headspinnerd <kouairchair@gmail.com>
2021-08-03 22:22:52 +0900

    Fixed deprecated property

--- ConveniApp/Models/Extension/UIDevice+SafeArea.swift
+++ ConveniApp/Models/Extension/UIDevice+SafeArea.swift
@@ -10,8 +10,12 @@ import SwiftUI
 extension UIDevice {
     /// Returns `true` if the device has a notch
     var hasNotch: Bool {
-        // TODO: need to fix deprecated property "windows"
-        guard let window = UIApplication.shared.windows.filter({$0.isKeyWindow}).first else { return false }
+        guard let window = UIApplication.shared.connectedScenes
+                .filter({$0.activationState == .foregroundActive})
+                .map({$0 as? UIWindowScene})
+                .compactMap({$0})
+                .first?.windows
+                .filter({$0.isKeyWindow}).first else { return false }
         if UIDevice.current.orientation.isPortrait {
             return window.safeAreaInsets.top >= 44
         } else {
