344d5ce67c441a5e3a1fbdb8eed586a828cb8b5d
tanakabp <tanaka@bp.i3-systems.com>
2022-08-19 00:38:42 +0900

    Added Yahoo News

---
+++ ConveniApp/Models/News/YahooNewsParam.swift
@@ -0,0 +1,14 @@
+//
+//  NewsParam.swift
+//  ConveniApp
+//
+//  Created by tanakabp on 2022/08/18.
+//
+
+import Foundation
+
+struct YahooNewsParam {
+    var Url: String
+    var AuthorName: String
+    var fetchesOnlyToday: Bool
+}
